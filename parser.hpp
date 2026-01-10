d0b5c6e317e1d686ada26b2822403ecc7be06ff1
Toko Tskhadiashvili <tskhadiashvili.toko@gmail.com>
2026-01-10 19:49:07 +0400

    Update parser.hpp

--- parser.hpp
+++ parser.hpp
@@ -7,7 +7,14 @@
 #include <string>
 #include <map>
 
-std::map<std::string, std::string> parse_config(std::string& path) {
+std::string _TrimString(const std::string& s) {
+	size_t start = s.find_first_not_of(" \t\r\n");
+	size_t end = s.find_last_not_of(" \t\r\n");
+	if (start == std::string::npos || end == std::string::npos) return "";
+	return s.substr(start, end - start + 1);
+}
+
+std::map<std::string, std::string> ParseConfig(const std::string& path) {
 	std::ifstream file(path);
 	std::map<std::string, std::string> config;
 
@@ -17,12 +24,14 @@ std::map<std::string, std::string> parse_config(std::string& path) {
 
 	std::string line;
 	while (std::getline(file, line)) {
-		if (line.empty() || line[0] == '#')
+		if (line.empty() || line[0] == '#') {
 			continue;
+		}
 
 		auto position = line.find('=');
-		if (position == std::string::npos)
+		if (position == std::string::npos) {
 			continue;
+		}
 
 		std::string key = line.substr(0, position);
 		std::string value = line.substr(position + 1);
@@ -31,18 +40,4 @@ std::map<std::string, std::string> parse_config(std::string& path) {
 	}
 
 	return config;
-}
-
-std::vector<std::string> parse_directory_list(std::string& value) {
-	std::vector<std::string> result;
-	std::stringstream ss(value);
-	std::string directory;
-
-	while (std::getline(ss, directory, ';')) {
-		if (!directory.empty()) {
-			result.push_back(directory);
-		}
-	}
-
-	return result;
 }
